98c7d0b3737079063bb4b3f0aa2af4c529e778f1
soponbeng <soponbeng@gmail.com>
2021-06-05 15:38:03 +0700

    Create Exercise4_soponwit_s.py

---
+++ Exercise4_soponwit_s.py
@@ -0,0 +1,9 @@
+FE = 85.5
+GB = 75
+ICS = 69.5
+ComP = 71
+print("--- Your Score ---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",ComP)
